7b87aee72ccebe5d977aa675fcb5b57ef7e13ddc
Renz Hernandez <mrchezco1995@gmail.com>
2020-11-10 15:08:30 +0800

    Add files via upload

---
+++ ObjectOrientedProgram Sample/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectOrientedProgram_Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            asdffunctions.Calcheight(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your Height in Inches is " + asdffunctions.ConvHeight(Convert.ToInt32(textBox1.Text)));
+        }
+
+        
+    }
+}
---
+++ ObjectOrientedProgram Sample/asdffunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectOrientedProgram_Sample
+{
+    public class asdffunctions
+    {
+        public static void messagepop()
+        {
+            MessageBox.Show("Hello World");
+        }
+
+        public static void Calcheight(String height)
+        {
+            MessageBox.Show("Your Height is " + height + " CM");
+        }
+
+        public static double ConvHeight(int cm)
+        {
+            double inch = cm * 0.393701;
+            return inch;
+        }
+    }
+}
